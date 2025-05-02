ad55f38fd2b0e151e56d1a48949dcd8d2a15fa2c
Chris W <363708+christopherwoodall@users.noreply.github.com>
2025-05-02 15:20:16 -0500

    update web app

--- yolov8-tfjs/src/App.jsx
+++ yolov8-tfjs/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   const canvasRef = useRef(null);
 
   // model configs
-  const modelName = "yolov8n";
+  const modelName = "yolov8n-four-leaf";
 
   useEffect(() => {
     tf.ready().then(async () => {
